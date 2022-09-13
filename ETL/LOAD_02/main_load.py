e58f1b575a9bf7f993271035816361ed8053584e
Daniel Lopez <daniel.lopez.pajares.2021@gmail.com>
2022-09-13 13:43:51 +0200

    update_etl_files

---
+++ ETL/LOAD_02/main_load.py
@@ -0,0 +1,13 @@
+import pymongo
+import sys
+
+try:
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+except Exception as e:
+    print('Connection could not be done' + str(e))
+    sys.exit()
+
+db = client['GW2_SRS']
+collection = db['players_info']
+
+mongo_insert = collection.insert_one()
